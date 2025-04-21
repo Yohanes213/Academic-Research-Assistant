3b896fb7f5f7486301709ba225ea27857244f5be
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2025-04-21 17:32:52 +0300

    chore: Clean up unused lines in Airflow DAG for paper ingestion

--- airflow/dags/paper_ingestion_dag.py
+++ airflow/dags/paper_ingestion_dag.py
@@ -5,12 +5,6 @@ from datetime import datetime, timedelta
 import sys
 import os
 
-# sys.path.append(os.path.join(os.path.dirname(__file__), '../../paper_ingestion'))
-# sys.path.append('/opt/airflow/paper_ingestion')
-
-
-# from historical_scrapper import scrape_articles
-
 
 default_args = {
     'owner': 'airflow',
@@ -18,7 +12,8 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'start_date': datetime(2025, 4, 5),
+    'retry_delay': timedelta(seconds=10),
+    'start_date': datetime(2025, 4, 21),
     'schedule_interval': '@daily',
     'catchup': False
 }
@@ -30,23 +25,16 @@ dag = DAG(
 )
 
 
-# def run_scrapper():
-#     scrape_articles()
-
-
-# scrap_task = PythonOperator(
-#     task_id='scrap_task',
-#     python_callable=scrape_articles,
-#     dag=dag,
-# )
 
 scrap_task = SimpleHttpOperator(
     task_id='scrap_task',
-    # http_conn_id='scrapper_api',  # You'll define this in Airflow connections
+    http_conn_id=None,
     endpoint='http://paper-ingestion:8000/scrape',
     method='GET',
-    http_conn_id=None,
+    headers={"Content-Type": "application/json"},
     dag=dag,
+    retries=3,
+    retry_delay=timedelta(seconds=10),
 )
 
 
